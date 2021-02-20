f2b77015d6890fe400b602423204553c7b3cf533
Liang <liang@qq.com>
2021-02-20 11:34:22 +0800

    only main

--- GitTest/GitTest/main.cpp
+++ GitTest/GitTest/main.cpp
@@ -4,8 +4,14 @@ using namespace std;
 
 int main()
 {
+	int v1 = 100;
+	int v3 = 80;
+	//*(int *)(v1 + 412) = v3;
+
 	Person a;
 	a.setName('B');
+	cout << a.GetName() << endl;
+	a.setName('h');
 	cout << a.GetName() << endl;
 	return 0;
 }
